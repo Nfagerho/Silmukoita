c594f6d1c083c86a07ab1a5d980baef7ca6ca741
Nfagerho <Nooa.Fagerholm@wippies.fi>
2018-09-18 19:51:53 +0300

    Lisää testiä

--- src/fi/academy/Silmukoita.java
+++ src/fi/academy/Silmukoita.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Silmukoita {
     public static void main(String[] args) {
         Scanner lukija = new Scanner(System.in);
-        System.out.println("Tämä on testiä GIT:iin");
+        System.out.println("Tämä on testiä GIT:iin. Nyt lisäsin tähän tämän lauseen.");
         System.out.println("Tässä pieni silmukka:");
 
         int i = 0;
